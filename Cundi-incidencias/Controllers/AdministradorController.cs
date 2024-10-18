a687b75b39b91d6abd67db6183aed9c74503d026
Yvr2812 <vanessara2812@gmail.com>
2024-10-18 00:54:00 -0500

    Se crea el controller de administrador

---
+++ Cundi-incidencias/Controllers/AdministradorController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cundi_incidencias.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AdministradorController : ControllerBase
+    {
+    }
+}
